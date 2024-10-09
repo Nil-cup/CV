14c238b6e6076185f00b180eba229d6d95499f1a
Nil-cup <nsastre20625@iesjoanramis.org>
2024-10-09 13:23:58 +0200

    Add files via upload

---
+++ cv/js/script.js
@@ -0,0 +1,8 @@
+function canviNom(){
+    let nombre = document.getElementById("nombre").value;
+    document.getElementById("name").innerHTML = nombre  
+}   
+{
+    let nombre = document.getElementById("color").value;
+    document.getElementById("color").innerHTML = nombre
+}
